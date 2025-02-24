cef5a0174889ebe9bf12e892f5a0741e564e60c5
Sucharitha Reddy R <sucharitha6711@gmail.com>
2025-02-24 12:06:13 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,4 @@
+document.getElementById("cvDownloadBtn").addEventListener("click", function() {
+    const cvUrl = "https://drive.google.com/file/d/1iAjMk2qMXPIOgHYLEhCKGv9sb2CNG-jo/view?usp=drive_link";
+    window.location.href = cvUrl;
+});
